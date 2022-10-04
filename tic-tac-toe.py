213f124e637dcd41e0c4814f544157adc94e05e2
JoInCp <kimdang185@gmail.com>
2022-10-05 01:12:56 +0900

    #3

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,10 +1,16 @@
 import pygame 
 
+WHITE = (255, 255, 255)
+RECT_SIZE = 150
+BLANK_SIZE = 25
+
 pygame.init()
 
 window = pygame.display.set_mode((550, 550))
 
 pygame.display.set_caption('Tic-Tac-Toe')
+
+matrix = [[[pygame.draw.rect(window, WHITE, (BLANK_SIZE * (col + 1) + RECT_SIZE * col, BLANK_SIZE * (row + 1) + RECT_SIZE * row, RECT_SIZE, RECT_SIZE)), -1] for col in  range(3)] for row in range(3)]
 
 run = True
 
